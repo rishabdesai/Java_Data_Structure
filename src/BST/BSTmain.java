db791f8402bf2702187208ab3ee3c54938259836
desai.rishab <desai.rishab@gmail.com>
2021-07-19 09:44:25 +0530

    Comments updated

--- src/BST/BSTmain.java
+++ src/BST/BSTmain.java
@@ -22,9 +22,6 @@ class BinarySearchTree {
 		public String toString() {
 			return "Node [data=" + data + "]";
 		}
-		
-		
-
 	}
 
 	private Node root;
@@ -33,6 +30,7 @@ class BinarySearchTree {
 		this.root = null;
 	}
 
+//1. Add Values to BST	(using non-recursion)
 	public void add(int val) {
 		Node nn = new Node(val);
 		if (root == null)
@@ -57,28 +55,30 @@ class BinarySearchTree {
 		}
 	}
 
-//	private void addRecursive(Node trav, int val) {
-//		if (val < trav.data) {
-//			if (trav.left == null)
-//				trav.left = new Node(val);
-//			else
-//				addRecursive(trav.left, val);
-//		} else {
-//			if (trav.right == null)
-//				trav.right = new Node(val);
-//			else
-//				addRecursive(trav.right, val);
-//		}
-//
-//	}
-//
-//	public void addRecursive(int val) {
-//		if (root == null)
-//			root = new Node(val);
-//		else
-//			addRecursive(root, val);
-//	}
+	//1a. Add Values to BST	(using recursion)	
+/*	private void addRecursive(Node trav, int val) {
+		if (val < trav.data) {
+			if (trav.left == null)
+				trav.left = new Node(val);
+			else
+				addRecursive(trav.left, val);
+		} else {
+			if (trav.right == null)
+				trav.right = new Node(val);
+			else
+				addRecursive(trav.right, val);
+		}
+	}
 
+	public void addRecursive(int val) {
+		if (root == null)
+			root = new Node(val);
+		else
+			addRecursive(root, val);
+	} */
+
+//2. find recursive preOrder, inOrder, postOrder
+	//recursive preorder
 	private void recPreOrder(Node trav) {
 		if (trav == null)
 			return;
@@ -89,95 +89,101 @@ class BinarySearchTree {
 	}
 
 	public void recPreOrder() {
+		System.out.println();
 		System.out.print("pre-order values : ");
 		recPreOrder(root);
 	}
-	
+
+	//recursive inorder
 	private void recInOrder(Node trav) {
 		if (trav == null)
 			return;
-		
+
 		recInOrder(trav.left);
 		System.out.print(trav.data + " ");
 		recInOrder(trav.right);
 	}
 
 	public void recInOrder() {
+		System.out.println();
 		System.out.print("In-order values : ");
 		recInOrder(root);
 	}
-	
+
+	//recursive postorder
 	private void recPostOrder(Node trav) {
 		if (trav == null)
 			return;
-		
+
 		recPostOrder(trav.left);
 		recPostOrder(trav.right);
 		System.out.print(trav.data + " ");
 	}
 
 	public void recPostOrder() {
+		System.out.println();
 		System.out.print("Post-order values : ");
 		recPostOrder(root);
 	}
-	
-	//Delete Tree 
-	private void delTree(Node trav) {
-		if(trav ==null)
-			return;
-		delTree(trav.left);
-		trav.left=null;
-		delTree(trav.right);
-		trav.right=null;
-	}
 
-	public void delTree() {
-		delTree(root);
-		root=null;
-	}
-
-	//calculate the height of tree
+//3. find height of a tree
 	private int recHeight(Node trav) {
-		if(trav == null)
+		if (trav == null)
 			return -1;
 		int htLeft = recHeight(trav.left);
 		int htRight = recHeight(trav.right);
 		int max = Integer.max(htLeft, htRight);
 		return max + 1;
 	}
+
 	public int recHeight() {
 		return recHeight(root);
 	}
-	
-// search (recursion)
+
+//4. search element ( using recursion)
 	private Node recSearch(Node trav, int val) {
-		if(trav ==null)
+		if (trav == null)
 			return null;
-		if(val ==trav.data)
+		if (val == trav.data)
 			return trav;
-		if(val <trav.data)
+		if (val < trav.data)
 			return recSearch(trav.left, val);
 		else
 			return recSearch(trav.right, val);
 	}
-	
+
 	public Node recSearch(int val) {
 		return recSearch(root, val);
-	}
-	
-	//search (non-recursive)
-	public Node find(int val) {
-		Node trav= root;
-		while(trav != null) {
-			if(val == trav.data )
+	} 
+
+//4a. search element (using non-recursive)
+/*	public Node find(int val) {
+		Node trav = root;
+		while (trav != null) {
+			if (val == trav.data)
 				return trav;
-			if(val <trav.data)
+			if (val < trav.data)
 				trav = trav.left;
 			else
-				trav= trav.right;
+				trav = trav.right;
+		}
+		return null;
+	} */
+
+//5. Delete Tree
+	private void delTree(Node trav) {
+		if (trav == null)
+			return;
+		delTree(trav.left);
+		trav.left = null;
+		delTree(trav.right);
+		trav.right = null;
 	}
-	return null;
-}
+
+	public void delTree() {
+		delTree(root);
+		root = null;
+	}
 }
 
 public class BSTmain {
@@ -185,6 +191,7 @@ public class BSTmain {
 	public static void main(String[] args) {
 
 		BinarySearchTree bst = new BinarySearchTree();
+//1. Add values to BST
 		bst.add(50);
 		bst.add(30);
 		bst.add(70);
@@ -192,40 +199,32 @@ public class BSTmain {
 		bst.add(40);
 		bst.add(60);
 		bst.add(80);
-		
-		//
-		bst.recPreOrder();  	// 50 30 20 40 70 60 80 
-		System.out.println();
-		bst.recInOrder(); 		// 20 30 40 50 60 70 80
-		System.out.println();
-		bst.recPostOrder(); 	// 20 40 30 60 80 70 50 
+
+//2. find recursive preOrder, inOrder, postOrder
+		bst.recPreOrder(); // 50 30 20 40 70 60 80
+		bst.recInOrder(); // 20 30 40 50 60 70 80
+		bst.recPostOrder(); // 20 40 30 60 80 70 50
 		System.out.println();
 		
-		//find height of a tree
+//3. find height of a tree
 		System.out.println("--------------------");
-		System.out.println("height of a tree "+bst.recHeight());
-		System.out.println();
-		
-		// search
+		System.out.println("height of a tree : " + bst.recHeight());
+
+//4. search element ( using recursion)
 		System.out.println("--------------------");
-		System.out.println();
 		BinarySearchTree.Node temp = bst.recSearch(80);
-		if(temp == null)
+		if (temp == null)
 			System.out.println("not found");
 		else
-			System.out.println(" found "+temp);
-		
-		//delete tree
-				System.out.println("--------------------");
-				System.out.println("after deleting tree");
-				bst.delTree();
-				bst.recPreOrder();  	// 50 30 20 40 70 60 80 
-				System.out.println();
-				bst.recInOrder(); 		// 20 30 40 50 60 70 80
-				System.out.println();
-				bst.recPostOrder(); 	// 20 40 30 60 80 70 50 
-				System.out.println();
-		
-		
+			System.out.println(" found " + temp);
+
+//5. delete tree
+		System.out.println("--------------------");
+		System.out.println("after deleting tree : ");
+		bst.delTree();
+		bst.recPreOrder(); 
+		bst.recInOrder(); 
+		bst.recPostOrder(); 
+
 	}
 }
